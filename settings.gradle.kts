6532020b5699961bc4dcd65cc37d94c03befd333
Tomás <tj753134@gmail.com>
2026-07-10 22:02:33 +0300

    Fix: adiciona repositórios do Supabase e Ktor + correções gerais

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,6 +11,7 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
         maven("https://maven.pkg.jetbrains.space/public/p/ktor/eap")
+        maven("https://jitpack.io")
     }
 }
 rootProject.name = "MonitorApp"
